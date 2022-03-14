b2fad3e36a44f94fe83b824457931f584b5a7056
LucianoSF1992 <lucianosilvaferreira22@gmail.com>
2022-03-14 20:48:10 -0300

    Praticando Javascript

---
+++ ex2.js
@@ -0,0 +1,10 @@
+let nome = "Luciano"
+let idade = 29
+let usuarioAtivo = true
+ 
+console.log("Nome: ")
+console.log(nome)
+console.log("Idade: ")
+console.log(idade)
+console.log("Usuário está ativo no sistema?")
+console.log(usuarioAtivo)
