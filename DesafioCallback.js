d3de0906df24a528b9a4f23769b5a8939c5305d0
LucianoSF1992 <lucianosilvaferreira22@gmail.com>
2022-04-18 13:44:37 -0300

    Desafio Callback

--- DesafioCallback.js
+++ DesafioCallback.js
@@ -1,6 +1,22 @@
-/* Cria uma função chamada acaoCarro(), que receberá uma callback como
-parâmetro. Após isso crie duas funções de nome andar() - que deve exibir uma mensagem 
+/* Crie uma função chamada acaoCarro(), que receberá uma callback como
+parâmetro. Após isso, crie duas funções de nome andar() - que deve exibir uma mensagem 
 dizendo que o carro está andando e outra de nome parar() - que deve exibir uma mensagem dizendo que o 
 carro está parado.
 
 Execute ambas ações utilizando a função acaoCarro(), passando suas ações como callbacks. */
+
+function acaoCarro(callback){
+    callback();
+}
+
+function andar(){
+    console.log(`O carro está andando!`);
+}
+
+function parar(){
+    console.log(`O carro está parado!`);
+}
+
+
+acaoCarro(andar);
+acaoCarro(parar);
